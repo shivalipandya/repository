01e9dabda8aa0bb808296992d97a76bcbe250f09
Shivali Pandya <shivali.pandya024@gmail.com>
2019-05-30 09:52:57 -0400

    shivali's commit

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Shivali Pandya
  *  @version 1.1
  */
 public class Person4 {
@@ -31,7 +31,11 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      String modified = "";
+      for (int index = 0; index < input.length(); index++) {
+        modified += (char)(input.charAt(index) + 1)
+      }
+      return modified;
     }
     
     /**
